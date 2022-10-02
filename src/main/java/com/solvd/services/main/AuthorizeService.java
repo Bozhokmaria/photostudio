f67f98ae1e1984a5546a351de713c043485dad35
bozhokmaria <bozhokmaria@gmail.com>
2022-10-02 15:48:02 +0300

    login

--- src/main/java/com/solvd/services/main/AuthorizeService.java
+++ src/main/java/com/solvd/services/main/AuthorizeService.java
@@ -7,26 +7,65 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class AuthorizeService {
+
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
     ClientDAOImpl clientDAO = new ClientDAOImpl();
 
     private static final Logger LOGGER = LogManager.getLogger(AuthorizeService.class);
 
-    public void loginNewClient() {
+    public boolean loginNewClient() {
+
         Client client = new Client();
 
         Scanner scanner = new Scanner(System.in);
-        LOGGER.info("Type your First name");
-        String firstName = scanner.nextLine();
-        client.setFirstName(firstName);
-        LOGGER.info("Type your Last name");
-        String lastName = scanner.nextLine();
-        client.setLastName(lastName);
-        LOGGER.info("Type your email");
-        String email = scanner.nextLine();
-        client.setEmail(email);
+
+
+        boolean notValidFirstName = true;
+        while (notValidFirstName) {
+            LOGGER.info("Type your First name");
+            String firstName = scanner.nextLine();
+            if (firstName.matches("[A-Z][a-z]+")) {
+                client.setFirstName(firstName);
+                notValidFirstName = false;
+            } else {
+                LOGGER.error("First letter should be uppercase");
+            }
+        }
+
+
+        boolean notValidLastName = true;
+        while (notValidLastName) {
+            LOGGER.info("Type your Last name");
+            String lastName = scanner.nextLine();
+            if (lastName.matches("[A-Z][a-z]+")) {
+                client.setLastName(lastName);
+                notValidLastName = false;
+            } else {
+                LOGGER.error("First letter should be uppercase");
+            }
+        }
+
+
+        boolean notValidEmail = true;
+        while (notValidEmail) {
+            LOGGER.info("Type your email");
+            String email = scanner.nextLine();
+            Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
+            if (matcher.find()) {
+                client.setEmail(email);
+                notValidEmail = false;
+            } else {
+                LOGGER.error("not valid email entered");
+            }
+        }
+
+
         LOGGER.info("Type your password");
         String password = scanner.nextLine();
         client.setPassword(password);
@@ -34,19 +73,33 @@ public class AuthorizeService {
         List<Client> clients = clientDAO.getAll();
         if (clients.contains(client)) {
             LOGGER.error("Such client already exists");
+            return false;
         } else {
             clientDAO.add(client);
             LOGGER.info("Successful registration. Welcome! {} {} ", client.getFirstName(), client.getLastName());
+            return true;
         }
     }
 
 
-    public void loginOldClient() {
+    public boolean loginOldClient() {
+
         Client client = new Client();
         Scanner scanner = new Scanner(System.in);
-        LOGGER.info("Type your email");
-        String email = scanner.nextLine();
-        client.setEmail(email);
+        boolean notValidEmail = true;
+        while (notValidEmail) {
+            LOGGER.info("Type your email");
+            String email = scanner.nextLine();
+            Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
+            if (matcher.find()) {
+                client.setEmail(email);
+                notValidEmail = false;
+            } else {
+                LOGGER.error("not valid email entered");
+            }
+        }
+
+
         LOGGER.info("Type your password");
         String password = scanner.nextLine();
         client.setPassword(password);
@@ -60,6 +113,10 @@ public class AuthorizeService {
 
         if(inSystemClient != null){
             LOGGER.info("Successful login. Welcome! {} {} ", inSystemClient.getFirstName(), inSystemClient.getLastName());
+            return true;
+        } else {
+            LOGGER.error("No such client in the system");
+            return false;
         }
     }
 }
--- src/main/java/com/solvd/services/main/MainService.java
+++ src/main/java/com/solvd/services/main/MainService.java
@@ -9,8 +9,21 @@ public class MainService {
 
     private static final Logger LOGGER = LogManager.getLogger(MainService.class);
 
-    public void startApp(){
+    public void startApp() {
+        boolean loginSuccessful = false;
+
         Scanner scanner = new Scanner(System.in);
 
+        do {
+            LOGGER.info("Login as new client - type 1, login as regular - type 2");
+            int log = scanner.nextInt();
+            if (log == 1) {
+
+            } else if (log == 2) {
+
+            } else {
+
+            }
+        } while (loginSuccessful);
     }
 }
